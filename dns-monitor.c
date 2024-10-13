3ea9cfaa678be2de10e5d1d4bbff949ca1bcd381
Alisher <amazirinov@gmail.com>
2024-10-13 20:58:18 +0200

    added support of pcap files

--- dns-monitor.c
+++ dns-monitor.c
@@ -14,7 +14,8 @@
 
 volatile int running = 1;  // Флаг работы программы
 pcap_t *handle;            // Дескриптор pcap для захвата пакетов
-
+struct bpf_program fp;
+int in_offline = 0;
 // DNS Header Structure
 struct dns_header {
     unsigned short id; // идентификатор
@@ -24,6 +25,22 @@ struct dns_header {
     unsigned short nscount; // количество авторитетных записей
     unsigned short arcount; // количество дополнительных записей
 };
+
+void terminate_program(int signal){
+    if (signal == SIGINT || signal == SIGTERM || signal == SIGQUIT){
+        pcap_breakloop(handle);  // Прерывание цикла pcap
+        pcap_close(handle);
+        printf("Program terminated gracefully.\n");
+        pcap_freecode(&fp);
+        exit(EXIT_SUCCESS);
+    }else if (in_offline) {
+        //print_traffic_offline();
+        pcap_close(handle);
+        printf("Program terminated gracefully.\n");
+        pcap_freecode(&fp);
+        exit(EXIT_SUCCESS);
+    }
+}
 
 // Обработчик сигнала для завершения программы при нажатии Ctrl+C
 void handle_sigint(int sig) {
@@ -37,7 +54,7 @@ void packet_handler(u_char *args, const struct pcap_pkthdr *header, const u_char
     struct ip *iph = (struct ip *)(packet + 14);  // Смещение до IP заголовка
     struct udphdr *udph = (struct udphdr *)(packet + 14 + iph->ip_hl * 4);  // Смещение до UDP заголовка
     struct dns_header *dnsh = (struct dns_header *)(packet + 14 + iph->ip_hl * 4 + sizeof(struct udphdr)); 
-    
+
     char time_str[20]; 
     struct tm *ltime = localtime(&header->ts.tv_sec);
     strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", ltime);  
@@ -49,15 +66,15 @@ void packet_handler(u_char *args, const struct pcap_pkthdr *header, const u_char
     printf("DstPort: UDP/%d\n", ntohs(udph->uh_dport));
     printf("Identifier: 0x%04X\n", ntohs(dnsh->id));
     printf("Flags: QR=%d, OPCODE=%d, AA=%d, TC=%d, RD=%d, RA=%d, AD=%d, CD=%d, RCODE=%d\n",
-        (dnsh->flags >> 15) & 0x1, // QR
-        (dnsh->flags >> 11) & 0xF, // OPCODE
-        (dnsh->flags >> 10) & 0x1, // AA
-        (dnsh->flags >> 9) & 0x1,  // TC
-        (dnsh->flags >> 8) & 0x1,  // RD
-        (dnsh->flags >> 7) & 0x1,  // RA
-        (dnsh->flags >> 5) & 0x1,  // AD
-        (dnsh->flags >> 4) & 0x1,  // CD
-        dnsh->flags & 0xF           // RCODE
+        (ntohs(dnsh->flags) >> 15) & 0x1, // QR
+        (ntohs(dnsh->flags) >> 11) & 0xF, // OPCODE
+        (ntohs(dnsh->flags) >> 10) & 0x1, // AA
+        (ntohs(dnsh->flags) >> 9) & 0x1,  // TC
+        (ntohs(dnsh->flags) >> 8) & 0x1,  // RD
+        (ntohs(dnsh->flags) >> 7) & 0x1,  // RA
+        (ntohs(dnsh->flags) >> 5) & 0x1,  // AD
+        (ntohs(dnsh->flags) >> 4) & 0x1,  // CD
+        ntohs(dnsh->flags) & 0xF           // RCODE
     );
 
     printf("[Question Section]\n");
@@ -68,43 +85,94 @@ void packet_handler(u_char *args, const struct pcap_pkthdr *header, const u_char
     printf("====================\n");
 }
 
+pcap_t *handle_interface(char *interface){
+    char errbuf[PCAP_ERRBUF_SIZE];  
+    pcap_t *handle = NULL;
+    handle = pcap_open_live(interface, BUFSIZ, 1, 1000, errbuf);
+    if (handle == NULL) {
+        fprintf(stderr, "Couldn't open device: %s\n", errbuf);
+        exit(EXIT_FAILURE);
+    }
+
+    char filter_exp[] = "udp port 53";
+    if (pcap_compile(handle, &fp, filter_exp, 0, PCAP_NETMASK_UNKNOWN) == -1) {
+        fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
+        exit(EXIT_FAILURE);
+
+    }
+
+    if (pcap_setfilter(handle, &fp) == -1) {
+        fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
+        exit(EXIT_FAILURE);
+    }
+
+    return handle;
+}
+
+pcap_t *handle_pcap_file(char* pcapfile){
+    char errbuf[PCAP_ERRBUF_SIZE];  
+    pcap_t *handle = NULL;
+    handle = pcap_open_offline(pcapfile, errbuf);
+    if (handle == NULL) {
+        fprintf(stderr, "Couldn't open device: %s\n", errbuf);
+        exit(EXIT_FAILURE);
+    }
+    char filter_exp[] = "udp port 53";
+    if (pcap_compile(handle, &fp, filter_exp, 0, PCAP_NETMASK_UNKNOWN) == -1) {
+        fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
+        exit(EXIT_FAILURE);
+
+    }
+    if (pcap_setfilter(handle, &fp) == -1) {
+        fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
+        exit(EXIT_FAILURE);
+    }
+    return handle;
+}
+
+
+void start_monitoring(struct InputData input_data){
+    signal(SIGINT, terminate_program);
+    signal(SIGTERM, terminate_program);
+    signal(SIGQUIT, terminate_program);
+    printf("-i: %s\n-p: %s\n-v: %s\n-d: %s\n-t: %s\n", input_data.interface, input_data.pcapfile, 
+                                                        (input_data.verbose ? "true" : "false"),
+                                                        input_data.domainsfile, input_data.transfile);
+    
+    if(strcmp(input_data.interface, "none") != 0){
+        printf("interface: %s\n", input_data.interface);//delete
+        printf("=========\n");//delete
+        handle = handle_interface(input_data.interface);
+    }else{
+        printf("pcap file: %s\n", input_data.pcapfile);//delete
+        printf("=========\n");//delete
+        handle = handle_pcap_file(input_data.pcapfile);
+    }
+
+    
+    if(pcap_loop(handle, 0, packet_handler, NULL) == -1){
+        fprintf(stderr, "Error with pcap loop\n");
+    }
+
+    terminate_program(0);
+    //pcap_close(handle);
+    //printf("Program terminated gracefully.\n");
+    //pcap_freecode(&fp);
+}
+
 // Основная функция
 int main(int argc, char *argv[]) {
-    char errbuf[PCAP_ERRBUF_SIZE];  
-    struct bpf_program fp;
     struct InputData input_data;
 
     input_data = parse_arguments(argc, argv);
 
-    signal(SIGINT, handle_sigint);
+    //signal(SIGINT, handle_sigint);
 
     printf("argc: %d\n", argc);
-    printf("-i: %s\n-p: %s\n-v: %s\n-d: %s\n-t: %s\n", input_data.interface, input_data.pcapfile, 
-                                                        (input_data.verbose ? "true" : "false"),
-                                                        input_data.domainsfile, input_data.transfile);
-
-    handle = pcap_open_live("eth0", BUFSIZ, 1, 1000, errbuf);
-    if (handle == NULL) {
-        fprintf(stderr, "Couldn't open device: %s\n", errbuf);
-        return 2;
-    }
+    
+    start_monitoring(input_data);
 
     
-    char filter_exp[] = "udp port 53";
-    if (pcap_compile(handle, &fp, filter_exp, 0, PCAP_NETMASK_UNKNOWN) == -1) {
-        fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
-        return 2;
-    }
-    if (pcap_setfilter(handle, &fp) == -1) {
-        fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
-        return 2;
-    }
-
-    pcap_loop(handle, 0, packet_handler, NULL);  // 0 означает "бесконечно"
-
-    pcap_close(handle);
-    
-    printf("Program terminated gracefully.\n");
 
     return 0;
 }
--- parse_args.c
+++ parse_args.c
@@ -3,7 +3,8 @@
 #include "parse_args.h"
 
 // ./dns-monitor (-i <interface> | -p <pcapfile>) [-v] [-d <domainsfile>] [-t <translationsfile>]
-// ./dns-monitor -i eth0 -v -d text.txt -t text1.txt
+// sudo ./dns-monitor -i eth0 -v -d text.txt -t text1.txt
+// sudo ./dns-monitor -p test.pcapng -v -d text.txt -t text1.txt
 // max valid number of args: 6
 // min valid number of args: 3
 
